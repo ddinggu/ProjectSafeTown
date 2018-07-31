3b9e1c623ef0dfcbb41d1bb0ecca400cde323136
ginnyang <ginnayng@gmail.com>
2018-07-31 16:42:33 +0900

    merge web db

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,6 +1,30 @@
-module.exports = function(app,fs){
+module.exports = function(app,MongoClient){
+  var url = 'mongodb+srv://meow_db:mnbvcxz070809@ddinggu-m001-dgyfk.mongodb.net/cctv';
+
   //default 화면
   app.get('/',function(req,res){
     res.render('index', {title:"My homepage", length:5});
+  });
+
+  //db에서 값 불러오기 예시
+  app.get('/cctv',function(req,res){
+    MongoClient.connect(url,function(err,client){
+      var db = client.db('cctv');
+      var cursor = db.collection('geoSeoulcctv').find({
+        'properties.location':'성동구 '//동대문구,강동구,광진구,송파구
+      }).project({
+        '_id':0,
+        'geometry':1,//위도경도
+        //'properties':0//주소
+      });
+      cursor.toArray(function(err,item){//typeof item = "array"
+        if(err){
+          console.log(err);
+        }else{
+          res.send(item);
+          client.close();
+        }
+      });
+    });
   });
 }
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //default setting
 let express = require('express');
 let app = express();
-let fs = require('fs');
 let bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 //let session = require('express-session');
+//let fs = require('fs');
 
 let server = app.listen(3000,function(){
   console.log("server started on port 3000!");
@@ -18,7 +19,7 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
 //kibaek. routing enviroment for client main events
-let mainRouter = require('./router/mainRouter')(app,fs);
+let mainRouter = require('./router/mainRouter')(app,MongoClient);
 
 //gyungjin. routing enviroment for db associated events
 let User = require('./DBmodel/userSchema');
